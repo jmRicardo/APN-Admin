e513c671b17a28f1a93ac8489a2d6a2e7fce4a08
guimainini <56739920+guimainini@users.noreply.github.com>
2019-10-23 21:44:45 -0300

    soyguille y soy regroso

--- APN Admin/main.cpp
+++ APN Admin/main.cpp
@@ -5,6 +5,8 @@
 int main()
 {
     cell adl[30];
+    int opcion;
+    char control;
     /*loadPlayer("EnriquePinty");
     loadPlayer("AdrianBarba");
     loadPlayer("JoaquinAlvarez");
@@ -19,6 +21,38 @@ int main()
     //printArrayOfPlayers(adl, validos);
     listaToArrayOfPlayers(adl, validos);
     printArrayOfPlayersWithListas(adl, validos);
+
+    do{
+        printf("\n01- Cargar un jugador");  /// o modificar
+        printf("\n02- Listado de Players");
+        printf("\n03- Desactivar Player papu");
+        printf("\n04- Mostrar score de un Player");
+        printf("\n05- Mostrar Match determinado");
+        printf("\n06- Mostrar score de todos los Player");
+        printf("\n07- Mostrar todos los Match");
+        printf("\n08- Un salir");
+        printf("\n09- Mostrar los Players ordenados por alguna propiedad");
+
+
+        fflush(stdin);
+        scanf("%d", &opcion);
+
+        switch(opcion){
+        case 1:
+            system("cls");
+            break;
+        case 8:
+            exit(0);
+        }
+
+        printf("\nVolver al menu? (s/n)");
+        fflush(stdin);
+        scanf("%c", &control);
+    }while (control = 's');
+
+
+
+
 
     return 0;
 }
